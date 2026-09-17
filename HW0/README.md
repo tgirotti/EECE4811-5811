97227d43e9f4a23521729dcc01553f7ef2950a85
tgirotti <180780352+tgirotti@users.noreply.github.com>
2026-09-17 15:25:34 -0400

    Add comments and design info

--- HW0/README.md
+++ HW0/README.md
@@ -3,13 +3,13 @@
 **Q: Why can’t two ordinary processes simply communicate by reading and writing normal variables in each other’s address spaces?**
 * Because of process isolation and security, the kernel creates new virtual address s paces for each process that is separate from other processes. Allowing arbitrary read/write access to another process' address space would violate this principle.
 
-**Q: Suppose the Producer sends the numbers 1, 2, 3, 4, 5 through one pipe and the Consumer reads them. Does this alone guarantee the following printed output order?
-Producer: 1
-Consumer: 1
-Producer: 2
-Consumer: 2…
+**Q: Suppose the Producer sends the numbers 1, 2, 3, 4, 5 through one pipe and the Consumer reads them. Does this alone guarantee the following printed output order?**
+**Producer: 1**
+**Consumer: 1**
+**Producer: 2**
+**Consumer: 2…**
 
-Explain why or why not. What synchronization does your design need in order to guarantee this order?**
+**Explain why or why not. What synchronization does your design need in order to guarantee this order?**
 
 * The output ordering is not guaranteed based solely on producing and sending the numbers in that order. A producer may send numbers `1`, `2`, and `3` through the pipe via `write()`, but the consumer might only print `1` after it receives `3`. To sync, we can `fork()`, have the producer send a number, then call `waitpid()` on the child, and when the child exits, repeat the process so we don't send until the number has been consumed.
 
@@ -22,3 +22,20 @@ Explain why or why not. What synchronization does your design need in order to g
 
 **QWhy is it good practice for each process to close pipe ends that it does not use?**
 * It is good practice because unclosed pupes will lead to deadlocks and hangs.
+
+# Running
+Execute the following command on a Unix-based or Unix-like system with `gcc` to run the program. Optimizations are disabled so we can learn from the disassembly of the program:
+```bash
+gcc -O0 main.c hw0
+chmod +x hw0
+./hw0
+```
+
+# Design
+The program uses a producer/consumer setup to print numbers. Two pipes are created: one for the Producer -> Consumer (numbers pipe), and one for the Consumer -> Producer pipe. The producer will fork itself after creating the pipes to create the consumer child. The producer will send numbers by writing to the `p2c_pipe` and wait for the child to write the acknowledgement byte via `c2p_pipe` before sending the next number. The consumer reads numbers from `p2c_pipe`. The producer will execute the number-sending sequence 5 times, and the consumer will execute the number-reading/acking sequence 5 times before closing the pipes and exiting.
+
+The main idea behind this is that each part will block until the ordering of numbers can be guaranteed. That is, the producer prints out the number, then waits for the consumer to do the same before printing out the next number, until all numbers have been printed.
+
+# Dependencies
+
+A POSIX-compliant system using `glibc` (or another compliant standard library, but this is not guaranteed to work) is required. The `#include` statements in `main.c` specify which syscalls are associated with each header.
--- HW0/main.c
+++ HW0/main.c
@@ -1,79 +1,90 @@
+// EECE.4811/5811
+// HW0
+// 9/17/2026
+// Nicholas Mankoski, Thomas Girotti
 
-#include <stdlib.h>
-#include <assert.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <stdio.h>
+#include <stdlib.h> // exit()
+#include <assert.h> // assert()
+#include <sys/wait.h> // waitpid()
+#include <unistd.h> // POSIX API (ssize_t)
+#include <stdio.h> // printf()
 
-int main(){
-  int p2c_pipe[2]; //producer to consumer pupe
-  int c2p_pipe[2]; //consumer to producer pipe, sends ack.
+int main()
+{
+  int p2c_pipe[2]; // Producer -> Consumer Pipe (send numbers)
+  int c2p_pipe[2]; // Consumer -> Producer Pipe (send acks for more numbers)
 
-//both pipes
-
+  // Instantiate pipes
   int r1 = pipe(p2c_pipe);
   int r2 = pipe(c2p_pipe);
-  assert(r1 == 0 && r2 == 0);
+  assert(r1 == 0 && r2 == 0); // Did pipes succeed?
 
-//maing child proc.
+  // Create child process by forking
   pid_t p1 = fork();
-  assert(p1 >= 0);  
+  assert(p1 >= 0); // Did we succeed in forking?
 
-  if(p1 == 0){//consmer proces/child
+  if (p1 == 0) // We are the child, we are the consumer
+  { 
+    // Close unused ends since consumer doesn't write to datapipe and doesn't read from ack pipe (from Brown lecture)
     close(p2c_pipe[1]);
     close(c2p_pipe[0]);
-    //closing the pipes the other doesnt use as meentioned in hw. 
-    //consmer doesnt wrtie to data and consumer doesnt read from ack pipe
-    for(int i =1; i <=5; i++){
-      //read bloacks of data till the producer writes to the pipe
-      int prod_num;//producer num given
 
-      //block prducer until next int
+    // Read 5 times from the producer pipe
+    for (int i = 1; i <= 5; i++)
+    {
+      int prod_num; // Producer number
+
+      // Block with read until we receive a number from the producer via the pipe
       ssize_t byteread = read(p2c_pipe[0], &prod_num, sizeof(prod_num));
-      assert(byteread == sizeof(prod_num));
-      
-      printf("Consumer: %d\n", prod_num);
-      // and than sends ack byte back to the produscer
+      assert(byteread == sizeof(prod_num)); // Make sure we read an integer
+
+      printf("Consumer: %d\n", prod_num); // We got the number, print it out
+      // Send acknowledgement byte back to producer so we receive more numbers
       char ack = 'k';
+      // Write to write end of Consumer -> Producer pipe
       ssize_t bytewrite = write(c2p_pipe[1], &ack, sizeof(ack));
-      assert(bytewrite == sizeof(ack));
-      
+      assert(bytewrite == sizeof(ack)); // Ensure we successfully wrote acknowledgement byte
     }
+
+    // Finished with receiving and printing numbers, close the file descriptors for our used pipe ends
     close(p2c_pipe[0]);
     close(c2p_pipe[1]);
+
+    // Exit child with success
     exit(0);
-    //closes the pipe ends and exits
-  } else{
-
-
+  }
+  else
+  { // We are the parent, not the forked child
+    // Close read end of Producer -> Consumer and write end of Consumer -> Producer
+    // (we don't read our own pipe and we don't write to the consumer's pipe)
     close(p2c_pipe[0]);
     close(c2p_pipe[1]);
 
+    // Send 5 numbers to the consumer
+    for (int i = 1; i <= 5; i++)
+    {
+      printf("Producer: %d\n", i); // Print the number to send
 
-    for(int i=1; i <=5; i++){
-      
-      printf("Producer: %d\n", i);
+      // Write the number we're sending to the pipe
+      ssize_t bytewrite = write(p2c_pipe[1], &i, sizeof(i));
+      assert(bytewrite == sizeof(i)); // Make sure we actually wrote the number
 
-        // sends num thru pipe to consumer
-        ssize_t bytewrite = write(p2c_pipe[1], &i, sizeof(i));
-        assert(bytewrite == sizeof(i));
-
-        char ack;
-        ssize_t byteread = read(c2p_pipe[0], &ack, sizeof(ack));
-        assert(byteread == sizeof(ack));
-      
-      
+      // Wait to send the next number until the consumer has acknowledged that we sent it over (from the other pipe)
+      char ack;
+      ssize_t byteread = read(c2p_pipe[0], &ack, sizeof(ack));
+      assert(byteread == sizeof(ack));
     }
-  
+
+    // Producer: Finished with sending numbers to the consumer. Close our used pipe ends
     close(p2c_pipe[1]);
     close(c2p_pipe[0]);
 
-
-      int status;
-      pid_t exited_pid = waitpid(p1, &status, 0);
-      assert(exited_pid == p1);
-      //wait for consumer to exit and prevent zombie process
+    // The consumer child may still be running, so we have to wait for them (avoid zombie process)
+    int status;
+    pid_t exited_pid = waitpid(p1, &status, 0);
+    assert(exited_pid == p1);
   }
 
-return 0;  
+  // Everything cleaned up, make sure we exit successfully
+  return 0;
 }
